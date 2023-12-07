e9fdc30ae425b76ac7b770e89dd0b107ceeb7b08
Yaroslav Shtanko <81259415+Yaroslav1918@users.noreply.github.com>
2023-12-07 16:10:05 +0200

    fix axios errors

--- src/components/googleLoginButton/GoogleLoginButton.tsx
+++ src/components/googleLoginButton/GoogleLoginButton.tsx
@@ -1,40 +1,11 @@
-import { GoogleLogin, useGoogleLogin } from "@react-oauth/google";
-import { Button } from "@mui/material";
-import { AxiosError } from "axios";
+import { GoogleLogin } from "@react-oauth/google";
 
-import { Colors } from "../../styles";
-import GoogleIcon from "@mui/icons-material/Google";
-import { GoogleInfo } from "../../types/GoogleProfile";
-import requestToGoogle from "../../utils/requestToGoogle";
 import { useAppDispatch } from "../../hooks/useAppDispatch";
 import { fetchGoogleLogInAsync } from "../../redux/user/userOperations";
+import { toast } from "react-toastify";
 
-
-interface GoogleLoginButtonProps {
-  
-}
-
-const GoogleLoginButton: React.FC<GoogleLoginButtonProps> = ({
-}) => {
-   const dispatch = useAppDispatch();
-  const login = useGoogleLogin({
-    onSuccess: async (response) => {
-      console.log(
-        "🚀 ~ file: GoogleLoginButton.tsx:19 ~ onSuccess: ~ response:",
-        response
-      );
-      try {
-        const res = await requestToGoogle(response);
-
-        // const { name, email, picture } = res;
-        // sendGoogleInfo({ name, email, picture });
-      } catch (e) {
-        const error = e as AxiosError;
-        return error.message;
-      }
-    },
-  });
-
+const GoogleLoginButton = () => {
+  const dispatch = useAppDispatch();
   return (
     <>
       <GoogleLogin
@@ -43,42 +14,16 @@ const GoogleLoginButton: React.FC<GoogleLoginButtonProps> = ({
             marginTop: "20px",
           },
         }}
-       width= "590"
+        width="590"
         onSuccess={(credentialResponse) => {
           if (credentialResponse) {
-            console.log(
-              "🚀 ~ file: GoogleLoginButton.tsx:43 ~ credentialResponse:",
-              credentialResponse
-            );
             dispatch(fetchGoogleLogInAsync(credentialResponse.credential));
-            // sendGoogleInfo(credentialResponse.credential);
           }
         }}
         onError={() => {
-          // Handle the Google login error here if needed
-          console.log("Login Failed");
+          toast.error("Login Failed");
         }}
       ></GoogleLogin>
-      <Button
-        type="submit"
-        variant="contained"
-        color="primary"
-        sx={{
-          marginTop: "20px",
-          padding: "7px ",
-          background: Colors.googleBack,
-          color: Colors.white,
-          "&:hover": {
-            background: Colors.googleHoverColor,
-          },
-        }}
-        onClick={async () => {
-          await login();
-        }}
-      >
-        <GoogleIcon sx={{ marginRight: "5px" }} />
-        Sign In using your Google account
-      </Button>
     </>
   );
 };
--- src/utils/extractErrorMessages.ts
+++ src/utils/extractErrorMessages.ts
@@ -1,23 +1,13 @@
 import { AxiosError } from "axios";
-import { ReactElement, JSXElementConstructor, ReactNode, ReactPortal } from "react";
-import { ToastContentProps, toast } from "react-toastify";
+import { toast } from "react-toastify";
 
 const extractErrorMessages = (e: unknown) => {
   const error = e as AxiosError;
   const errorResponse = JSON.parse(error.request.response);
-  console.log("🚀 ~ file: extractErrorMessages.ts:7 ~ extractErrorMessages ~ errorResponse:", errorResponse.issues)
-  if (typeof errorResponse.message === "string") {
-    toast.error(errorResponse.message);
-    return errorResponse.message;
-  } else if (Array.isArray(errorResponse.message)) {
-    const errorMessage = errorResponse.message.join(", ");
-    toast.error(errorMessage);
-    return errorMessage;
-  } else if (Array.isArray(errorResponse.issues)) {
-    errorResponse.issues.map((i: { message: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | ((props: ToastContentProps<unknown>) => ReactNode) | null | undefined; errorMessage: any; }): any => {
-      toast.error(i.message);
-      return i.errorMessage;
-    })
-  }
+  return errorResponse.issues.map((i: any) => {
+    const errMessage = `Field: ${i.path[1]}; Reason: ${i.message}`;
+    toast.error(errMessage);
+    return errMessage;
+  });
 };
 export default extractErrorMessages;
